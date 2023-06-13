e7ca07f99f6f8344ea5624d0141b8df5a7bbff20
oustrix <50222966+oustrix@users.noreply.github.com>
2023-06-14 02:21:49 +0300

    add watch functions
    WatchWithInterval(context.Context, time.Duration) creates a string channel and returns it.
    Also it starts another function in goroutine,
    which calls the Read function every interval(time.Duration from function parameters) and
    send clipboard data to channel if it is not equal to the last received data.
     It stops when it receives a signal from ctx.
    
    Watch(context.Context) just returning string channel from WatchWithInterval() by passing ctx and baseWatchInterval.
    
    Also add baseWatchInterval variable, it equals time.Second * 1 and write tests for Watch().

--- glippy_test.go
+++ glippy_test.go
@@ -1,6 +1,9 @@
 package glippy
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
 func TestSetGet(t *testing.T) {
 	tests := []struct {
@@ -39,6 +42,45 @@ func TestSetGet(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWatch(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "abc",
+			want: "abc",
+		},
+		{
+			name: "empty",
+			want: "",
+		},
+		{
+			name: "emoji",
+			want: "🔥",
+		},
+		{
+			name: "utf8",
+			want: "aš tave myliu",
+		},
+	}
+
+	ch := Watch(context.Background())
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Set(tt.want)
+			if err != nil {
+				return
+			}
+			got := <-ch
+			if got != tt.want {
+				t.Errorf("Watch() = %v, want %v", got, tt.want)
 			}
 		})
 	}
